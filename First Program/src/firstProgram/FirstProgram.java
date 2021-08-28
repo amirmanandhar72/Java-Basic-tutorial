d641d9c7e70dd748472614b7beb73504ab9f7922
amirm <amirm@DESKTOP-SQCUA3P>
2021-08-28 16:01:34 +0545

    Basics of java upto strings

---
+++ First Program/src/firstProgram/FirstProgram.java
@@ -0,0 +1,34 @@
+package firstProgram;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("This is me coding");
+		//Variables
+		// byte < short < int < <long <float < double 	
+		//logical operators & ,|
+		//if else
+		//looping
+		//function
+		//Arrray syntax
+		//char [] arr= new char []	
+		//Adding two arrays 
+		//Strings
+		int b=10;
+		String a= "he is good";
+		String d= new String("amir");
+		String c= "amir";
+		//String methods 
+			//charAt
+			//length
+			//toUpperCase toLowerCase
+			//startswith endswith
+			//string.valueof->converts int to string \
+		System.out.println(a+String.valueOf(b));
+		System.out.println(a.replace("he", "Amir"));
+		System.out.println(c.equals(d));// checks equals..
+		System.out.println(a.substring(2,8));// substring of a string
+	}
+}
+
--- Install.md
+++ Install.md
@@ -1,1 +1,14 @@
 # Instruction for setting up JAVA programming environment
+
+Download the JDK from the link given below.
+https://www.oracle.com/java/technologies/javase-downloads.html
+
+Download the IDE from the link 
+https://www.eclipse.org/downloads/
+
+Starting a Project
+Steps:
+1) Open ecllispe
+2) Create a new project 
+3) Create a new package 
+4) Finally create a new java class and then start your coding.
